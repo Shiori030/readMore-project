45ccd678033abd5019892197d43f1b08d94121eb
Shiori030 <nnn666nnn4125@gmail.com>
2024-12-06 17:07:49 +0800

    feat: web跟ios雙平台 文字行數抓取邏輯完成

--- src/components/ReadMore/index.js
+++ src/components/ReadMore/index.js
@@ -1,27 +1,38 @@
 import { useState, useRef, useEffect } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Platform } from "react-native";
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-
-export default function ReadMore({ title, content, lineLimit = 9999 }) {
+export default function ReadMore({ title, content, lineLimit = null }) {
   const [isOpen, setIsOpen] = useState(false);
   const [isMore, setIsMore] = useState(false);
-  const [newLine,setNewLine] = useState(null);
+  const [newLine, setNewLine] = useState(null);
   const [totalLine, setTotalLine] = useState(null);
   const contentRef = useRef();
 
   useEffect(() => {
-    const contentHeight = contentRef.current.clientHeight;   // 元素的可視高度
-    setTotalLine(contentHeight / 23);  // 高度除行高
+    if (Platform.OS === 'web') {
+      const contentDOM = contentRef.current;
+      const range = document.createRange();
+      range.selectNodeContents(contentDOM);
+      const rects = Array.from(range.getClientRects());
+      const rectsLines = new Set([]);
+      rects.forEach((line) => rectsLines.add(line.top))
+      setTotalLine(rectsLines.size);
+    }
   }, [])
 
   useEffect(() => {
-    if (totalLine > lineLimit) {
+    if (lineLimit &&  totalLine > lineLimit) {
       setIsMore(true);
       setNewLine(lineLimit);
     }
-  },[totalLine])
+  }, [totalLine])
+
+  const onTextLayout = (e) => {
+    if (Platform.OS !== 'web') {
+      const { lines } = e.nativeEvent;
+      setTotalLine(lines.length);
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -29,11 +40,11 @@ export default function ReadMore({ title, content, lineLimit = 9999 }) {
         <Text style={styles.title}>{title}</Text>
       </View>
       <View style={styles.contentArea}>
-        <Text numberOfLines={isOpen ? null : newLine} style={styles.content} ref={contentRef}>{content}</Text>
+        <Text numberOfLines={isOpen ? null : newLine} style={styles.content} ref={contentRef} onTextLayout={onTextLayout}>{content}</Text>
       </View>
       {isMore ? (
         <View style={styles.moreButton}>
-          <TouchableOpacity onPress={()=>setIsOpen(!isOpen)}><Text style={styles.buttonText}>{isOpen ? "Read more" : "Read less"}</Text></TouchableOpacity>
+          <TouchableOpacity onPress={() => setIsOpen(!isOpen)}><Text style={styles.buttonText}>{isOpen ? "Read less" : "Read more"}</Text></TouchableOpacity>
         </View>
       ) : null}
     </View>
@@ -42,22 +53,22 @@ export default function ReadMore({ title, content, lineLimit = 9999 }) {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#F0F0F0',
-    justifyContent: 'center',
-    width: windowWidth * 0.6,
+    width: Platform.OS === 'web' ? '60%' : '80%',
     paddingHorizontal: 15,
     borderRadius: 3,
+    marginHorizontal: 'auto',
   },
   title: {
     fontSize: 25,
+    textAlign: 'justify',
   },
   titleArea: {
     marginBlock: 15,
   },
   content: {
     fontSize: 16,
-    lineHeight: 23,
+    textAlign:"justify"
   },
   contentArea: {
     marginBottom: 10,
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,14 +1,35 @@
-import { View } from "react-native-web"
+import { ScrollView, View, StyleSheet } from "react-native"
 import ReadMore from "../../components/ReadMore"
+import { newsDatas } from "./newsDatas"
 
 export default function Home() {
   return (
-    <View>
-      <ReadMore
-        title={"TWICE回歸！ STRATEGY主打歌曝　粉絲讚「酷又甜美」"}
-        content={"TWICE 即將在12月6日發行迷你14輯《STRATEGY》，近日陸續傳出她們將合作歐美大咖歌手，除了已經宣布的美國饒舌歌手 Megan Thee Stallion(梅根尤物)外，還傳出 Coldplay 也是合作歌手之一， 尤其是 TWICE 將擔任 Coldplay 明年世巡首爾場的特別嘉賓，更是讓外界期待兩團的合作，而近日網上瘋傳 TWICE 這次新專輯歌單，竟然除了兩組歐美大咖外，還有 IU！TWICE 這次新專輯《STRATEGY》在發行前就討論度超高，而近日竟疑似專輯歌單外流，強大陣容也直接被瘋傳，除了同名歌曲〈STRATEGY〉將合作 Megan Thee Stallion 外，歌單上竟還出現與其他歌手的合作曲，與 Coldplay 合作的〈UMBRELLA〉，以及與 IU 合作的〈LULLABY〉，超強陣容火速引起熱議。雖然歌單真實性尚未被證實，但已經讓粉絲都超期待，紛紛留言表示「雖然不知道是不是真的，但有 IU 和 Coldplay 真的很扯...」、「喔買尬，這次陣容真的很大欸，雖然不知道是不是真的，但先小小相信一下」、「希望是真的!!!」、「IU 不清楚，但她們是明年 Coldplay 在韓國六場演唱會的嘉賓，如果12月的專輯有合作曲，當這六場的嘉賓就很合理」等，討論度不斷。"}
-        lineLimit={3}
-      />
+    <View style={styles.wrapper}>
+      <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+        {newsDatas.map((news) => (
+          <View key={news.id} style={styles.readMore}>
+            <ReadMore
+              title={news.title}
+              content={news.content}
+              lineLimit={news.lineLimit}
+            />
+          </View>
+        ))}
+      </ScrollView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  wrapper:{
+    flex: 1,
+    marginTop: 80,
+  },
+  readMore: {
+    marginTop: 20,
+  },
+  container: {
+    flex:1,
+    paddingBottom: 40,
+  }
+})
